855b56f13913afc1d1b7aaf743d02a8a83f52e99
leehuynhfvn <49875774+leehuynhfvn@users.noreply.github.com>
2020-03-10 13:51:32 +0700

    Update webspeed.sh

--- webspeed.sh
+++ webspeed.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 file=$1
 for i in `cat $file`; do
-	time=$(wget --user-agent="Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.136 YaBrowser/20.2.2.177 Yowser/2.5 Yptp/1.56 Safari/537.36" --timeout=10s -p tenten.vn -O /dev/null 2>&1 | tail -n2 | head -n1| awk '{print $5}')
+	time=$(wget --user-agent="Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.136 YaBrowser/20.2.2.177 Yowser/2.5 Yptp/1.56 Safari/537.36" --timeout=10s -p tenten.vn | tail -n2 | head -n1| awk '{print $5}')
+	rm -rf $i
 	echo "$i $time" >> kq.txt
 done
-	
