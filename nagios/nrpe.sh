00204ef2defeb0925c62e5a1e36d8868047e1729
leehuynhfvn <49875774+leehuynhfvn@users.noreply.github.com>
2020-09-03 13:58:11 +0700

    Update nrpe.sh

--- nagios/nrpe.sh
+++ nagios/nrpe.sh
@@ -1,4 +1,35 @@
+#!/bin/bash
+# Install Nagios agent on CentOS
+# Precheck
+if [[ -f /etc/nagios/nrpe.cfg ]]; then
+        echo "Nrpe already installed!"
+        exit 1
+fi
+
+
+if [ -z "$1" ]; then
+    echo "Please call '$0 <Nagios Server IP>' to run this command!"
+    exit 1
+fi
+
+# Installation
+# ---------------------------------------------------\
+
 yum install epel-release
-yum install nrpe nagios-plugins-all
+yum install nrpe -y
+mkdir -p /usr/lib64/nagios/plugins
+wget -P /usr/lib64/nagios/plugins -c https://raw.githubusercontent.com/leehuynhfvn/my-public-note/master/nagios/plugins.tar.gz -O - | tar -xz
+
+# Config nrpe
+sed -i "/^allowed_hosts/ s/$/\,$SERVER_IP/" /etc/nagios/nrpe.cfg
+sed -i "s/^NRPE_SSL_OPT=\"\"$/NRPE_SSL_OPT=\"-n\"/" /etc/sysconfig/nrpe
+
+# Configure firewalld
+# ---------------------------------------------------\
+firewall-cmd --permanent  --add-port=5666/tcp
+firewall-cmd --reload
+
+
+# Start service
 systemctl start nrpe.service
 systemctl enable nrpe.service
